4d093696c8793e55769c72f2d6109e96e8c2bb61
stjeanmark <52003247+stjeanmark@users.noreply.github.com>
2021-03-19 12:25:15 -0400

    Update tl_simple_inventory_tracker.php

--- system/modules/simple_inventory_tracker/languages/en/tl_simple_inventory_tracker.php
+++ system/modules/simple_inventory_tracker/languages/en/tl_simple_inventory_tracker.php
@@ -20,15 +20,15 @@ $GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['publish_legend'] 	= 'Publish
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['alias'] 			= array('Alias', 'Auto-generated alias.');
-$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['product_name'] 				= array('Product Name', 'The name of the product');
+$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['alias'] 			           = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['product_name'] 			   	= array('Product Name', 'The name of the product');
 $GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['product_inventory'] 		= array('Product Inventory', 'The inventory total');
-$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['published'] 		= array('Published', 'Save this product\'s record.');
+$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['published'] 		        = array('Published', 'Save this product\'s record.');
  
  /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['new']   			= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['new']   		 	= array('New record', 'Add a new record');
 $GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['show']  		 	= array('Record details', 'Show the details of record ID %s');
 $GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['edit']  		 	= array('Edit record', 'Edit record ID %s');
 $GLOBALS['TL_LANG']['tl_simple_inventory_tracker']['copy']  		 	= array('Copy record', 'Copy record ID %s');

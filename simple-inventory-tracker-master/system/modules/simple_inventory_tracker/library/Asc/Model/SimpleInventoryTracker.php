47be7fdbe79d9159259c84ca5d4337e16c3396a2
stjeanmark <52003247+stjeanmark@users.noreply.github.com>
2021-03-19 10:50:38 -0400

    Update SimpleInventoryTracker.php

--- simple-inventory-tracker-master/system/modules/simple_inventory_tracker/library/Asc/Model/SimpleInventoryTracker.php
+++ simple-inventory-tracker-master/system/modules/simple_inventory_tracker/library/Asc/Model/SimpleInventoryTracker.php
@@ -1,26 +1,26 @@
 <?php
 
 /**
- * Locations - Location Plugin for Contao
+ * Simple Inventory Tracker - A simple way to track inventory manually.
  *
- * Copyright (C) 2018 Andrew Stevens
+ * Copyright (C) 2021 Mark St. Jean.
  *
- * @package    asconsulting/locations
- * @link       http://andrewstevens.consulting
+ * @package    stjeanmark/simple_inventory_tracker
+ * @link       http://www.markstjean.com
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
  
 namespace Asc\Model;
 
-class Location extends \Model
+class SimpleInventoryTracker extends \Model
 {
 	
 	/**
 	 * Table name
 	 * @var string
 	 */
-	protected static $strTable = 'tl_location';
+	protected static $strTable = 'tl_simple_inventory_tracker';
 
 	
 }

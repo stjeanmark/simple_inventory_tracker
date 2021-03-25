fda89782f4f89986ffd1121931f76dcfb2c51f25
stjeanmark <52003247+stjeanmark@users.noreply.github.com>
2021-03-25 13:17:43 -0400

    Update SimpleInventoryTrackerInsertTag.php

--- system/modules/simple_inventory_tracker/library/CustomTags/SimpleInventoryTrackerInsertTag.php
+++ system/modules/simple_inventory_tracker/library/CustomTags/SimpleInventoryTrackerInsertTag.php
@@ -1,22 +1,36 @@
 <?php
 
-namespace CustomTags;
+	namespace CustomTags;
 
-class SimpleInventoryTrackerInsertTag
-{
-    public function onReplaceTag (string $insertTag)
-    {
-        if (stristr($insertTag, "::") === FALSE) {
-		return 'no_id';
+	class SimpleInventoryTrackerInsertTag
+	{
+		public function onReplaceTag (string $insertTag)
+		{
+			// if this tag doesnt contain :: it doesn't have an id, so we can stop right here
+			if (stristr($insertTag, "::") === FALSE) {
+				return 'no_id';
+			}
+
+			// break our tag into an array
+			$arrTag = explode("::", $insertTag);
+
+			// lets make decisions based on the beginning of the tag
+			switch($arrTag[0]) {
+				// if the tag is what we want, {{simple_inventory::id}}, then lets go
+				case 'simple_inventory':
+					// take our id, $arrTag[1], and pull our data out and return it
+
+					// for now, lets just return our ID to show we can get here
+					return $arrTag[1];
+				break;
+
+				// if we want to have other tags do other things they would go here
+			}
+
+			// this is an example from Andy for how I can pull that data out
+			// <Model_Name>::findBy('<field_name>', 'lookup_data');
+
+			// something has gone horribly wrong, let the user know and hope for brighter lights ahead
+			return 'something_went_wrong';
+		}
 	}
-		
-	$arrTag = explode("::", $insertTag);
-		
-        var_dump($arrTag);
-
-        //<Model_Name>::findBy('<field_name>', 'lookup_data');
-        
-        
-        return 'grabbed_and_returned';
-    }
-}
